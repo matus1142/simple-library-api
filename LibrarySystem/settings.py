5b2c61c1a3efc3d283073c16e82af19ef18a281f
Matus Lhongpol <matuslhongpol@gmail.com>
2024-08-12 11:03:56 +0700

    Add  - Auth Token at admin site  - Model at Admin site

--- LibrarySystem/settings.py
+++ LibrarySystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'LibrarySystemAPI',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- LibrarySystemAPI/admin.py
+++ LibrarySystemAPI/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from LibrarySystemAPI.models import Book, Loan, Member
+
 # Register your models here.
+admin.site.register([Member,Book,Loan])
